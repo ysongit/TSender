3359b6f3da1499e2a36f3600c7872d0d69cd63e8
Mr Web3 <87449884+ysongit@users.noreply.github.com>
2025-09-13 11:12:43 -0400

    add Tailwind CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           priority
         />
         <ol>
-          <li>
+          <li className="bg-red-300">
             Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
