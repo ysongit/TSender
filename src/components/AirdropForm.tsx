4350ca1699f5aac9017400ae8937a367d69da6bd
Mr Web3 <87449884+ysongit@users.noreply.github.com>
2025-09-19 12:27:47 -0400

    add more Input Fields

--- src/components/AirdropForm.tsx
+++ src/components/AirdropForm.tsx
@@ -5,6 +5,8 @@ import InputField from "./ui/InputField";
 
 export default function AirdropForm() {
   const [tokenAddress, setTokenAddress] = useState("");
+  const [recipients, setRecipients] = useState("");
+  const [amounts, setAmounts] = useState("");
 
   return (
     <div>
@@ -13,6 +15,20 @@ export default function AirdropForm() {
         placeholder="0x"
         value={tokenAddress}
         onChange={e => setTokenAddress(e.target.value)}
+      />
+      <InputField
+        label="Recipients"
+        placeholder="0x123, 0x124"
+        value={recipients}
+        onChange={e => setRecipients(e.target.value)}
+        large={true}
+      />
+      <InputField
+        label="Amount"
+        placeholder="100, 200"
+        value={amounts}
+        onChange={e => setAmounts(e.target.value)}
+        large={true}
       />
     </div>
   )
