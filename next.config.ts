e1e68e49b1a3392e1d8a20f1564e77798523d9c0
Mr Web3 <87449884+ysongit@users.noreply.github.com>
2025-10-28 16:36:30 -0400

    update next.config.ts to build as a static site

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  distDir: 'out',
+  images: {
+    // Optimized images only work on the server
+    unoptimized: true
+  },
+  basePath: '',
+  assetPrefix: './',
+  trailingSlash: true
 };
 
 export default nextConfig;
